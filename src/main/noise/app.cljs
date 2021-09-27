9baa1edd1ec0af3a0a7efa7deb2a0e7520df18d0
porkostomus <porkostomus@mail.com>
2021-09-27 04:16:19 -0700

    makes noise!

--- src/main/noise/app.cljs
+++ src/main/noise/app.cljs
@@ -1,4 +1,41 @@
-(ns noise.app)
+(ns noise.app
+  (:require
+   [reagent.core :as r]
+   [reagent.dom :as d]))
 
-(defn init []
-  (println "Hello World"))
+(defonce ^:dynamic *context* (js/AudioContext.))
+
+(defn audio-buffer [context duration]
+  (let [sample-rate 44100
+        frame-count (* sample-rate duration)
+        buffer (.createBuffer context 1 frame-count sample-rate)
+        data (.getChannelData buffer 0)]
+    (doseq [i (range frame-count)]
+      (aset data i (-> (js/Math.random) (* 2.0) (- 1.0))))
+    buffer))
+
+(defn buffer-source [buffer]
+  (let [source (.createBufferSource *context*)]
+    (.resume *context*)
+    (set! (.-buffer source) buffer)
+    (.connect source (.-destination *context*))
+    (.start source (.-currentTime *context*))
+    source))
+
+(defn button [label onclick]
+  [:button
+   {:on-click onclick}
+   label])
+
+(defn home-page []
+  [:div [button "Noise"
+         #(buffer-source (audio-buffer *context* 1.0))]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "root")))
+
+(defn ^:export init []
+  (mount-root))
